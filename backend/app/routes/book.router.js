5e1042f399c22dd5347defc3deb3025e1e39784b
Michelle Naim <michellenaim@gmail.com>
2021-11-29 18:33:31 -0800

    Fix delete

--- backend/app/routes/book.router.js
+++ backend/app/routes/book.router.js
@@ -1,9 +1,9 @@
 module.exports = function (app) {
   var books = require("../controllers/book.controller.js");
 
-  app.post("/api/posts", books.createBook);
-  app.get("/api/posts/:id", books.getBook);
+  app.post("/api/post", books.createBook);
+  app.get("/api/post/:id", books.getBook);
   app.get("/api/posts", books.books);
-  app.put("/api/posts", books.updateBook);
-  app.delete("/api/posts/:id", books.deleteBook);
+  app.put("/api/post", books.updateBook);
+  app.delete("/api/post/:id", books.deleteBook);
 };
